b60df0385d29aee349e1782beca3a61f824b14a0
Pratiksha <pt9413387@gmail.com>
2025-05-05 18:42:03 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const apiKey="c4944d282ab981d21e6fc0e4b7d49ac5";
+const apiURL ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+async function checkWeather(city) {
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    if(response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+    var data = await response.json();
+    
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "km/h";
+    if(data.weather[0].main == "Clouds") {
+        weatherIcon.src = "clouds.jpg";     
+    }
+    else if(data.weather[0].main == "Clear") {
+        weatherIcon.src = "clear.png";     
+    }
+    else if(data.weather[0].main == "Rain") {
+        weatherIcon.src = "rain.webp";     
+    }
+    else if(data.weather[0].main == "Mist") {
+        weatherIcon.src = "mist.png";     
+    }
+    else if(data.weather[0].main == "Snow") {
+        weatherIcon.src = "snow.jpg";     
+    }
+    else if(data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "drizzle.png";
+}
+document.querySelector(".weather").style.display = "block";
+document.querySelector(".error").style.display = "none";
+}
+}
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
